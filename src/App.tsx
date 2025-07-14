e6b5b7b9bcf52b7551f774fa84e7989ea45e7358
Ritik Nagpure <80842165+Ritik-Nagpure@users.noreply.github.com>
2025-07-14 20:35:48 +0530

    reinitialize

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+        <p className='text-9xl text-cyan-500 text-center font-bold'>
+          Arya
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
